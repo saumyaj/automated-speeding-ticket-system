f1a1599c9440a077d068acba3f61b9fbce37bc8e
saumyaj <saumya259@gmail.com>
2020-05-22 18:15:21 -0400

    removed cloud creds

--- SubscriberPeer/databaseManager.py
+++ SubscriberPeer/databaseManager.py
@@ -11,15 +11,9 @@ from CloudData import EMAIL_FUNCTION_URL
 from CloudData import SENDGRID_API_KEY
 
 OWNER_INFO_TABLE_NAME = 'OwnerInfo'
-
-
-aws_access_key_id='ASIA4KHZ3NQ4NIQTD5FP'
-aws_secret_access_key='6MdM1CNeZ1ZLuQybIfNo9LWAUkEbk+IWcdvykOYD'
-aws_session_token='FwoGZXIvYXdzEP///////////wEaDOrf2AfdMhRva4iiOCK9AUSTfg984aXxBPl3LtDboTTgopzluj43Ib+uhbz75PsMciOn+FJKLVNWH+vQiiQy9t04FF5QXL0/L4hL2iBLMXR8HOp4EV+P00DJqhMsOnu4G85DoRRrY1n37cSIgD3/jStiqUN+bXuy3m5JaRbxqzWQo7cEzD43gNC03JniUtjCDgusegzbKffbdJS9g3PyrPVqaW0O9I0GxCprX+0TNCQi5zJmzRy+lc11UcX8XTI+Jj9IK0Sd8dWXr08oKyjjn5/2BTIt8xmjkUucruAtC6QDV32BFonST+REIgMmwRaKBPkrot+p6HylfaCMiLjHF5SK'
-
-# aws_access_key_id = 'ASIA4KHZ3NQ4P5JC2ZUS'
-# aws_secret_access_key = 'eEtzu/2sltcGm5EHil5H7CnsMnM7EFN496WkvXNp'
-# aws_session_token = 'FwoGZXIvYXdzEPX//////////wEaDKlbxJ1iXv6ZjmtWtiK9ASNE56oltAdHmA9Vs5aYJAl0vGaPlhEjhTlmCygpzBW5zauzBRaJN5Osl39+yiyrusPXQldJ9RZsYjCPT6Og3IHDQfVk+VjOij2XBFuILmtNJqbiGn99njrxz0oES0It3yctKSMS1cXAQa00JNDVQ8ppfdEdp6OVT1LZhr20enGHTjLuy7xKiG3dIpLKm7OVJarZRcX0X7E695zNhlNgjYZXJTTCkBRTWO+PMkv+bKq8Hx51ewuPfcclmVPC5iiHnJ32BTItsfG7jHyLwPvE4N0pgkZJJ2xL+yccBEtHuegcqC3w4FMu3uAK51VKBUGegtq+'
+aws_access_key_id = 'AWS ACCESS KEY'
+aws_secret_access_key = 'AWS SECRET KEY'
+aws_session_token = 'AWS SESSION TOKEN'
 
 lambda_client = boto3.client('lambda',
                              aws_access_key_id=aws_access_key_id,
@@ -87,27 +81,6 @@ def process_fine(license_plate, timestamp, speed, image_url=''):
     add_ticket_to_db(json.dumps(payload))
 
 
-# def update_status():
-# ACCESS_KEY = "ASIA4KHZ3NQ4CZO3EDFF"
-# SECRET_KEY = "XtGEomqA0FohNS4qfqIVMvuTCb4IQELnTTWYg58h"
-# SESSION_TOKEN = "FwoGZXIvYXdzEOb//////////wEaDA7NdurNvfVw2CJg7yK9AZdw0qcyOmaIcYReRmTOxm9GGttJmOpOYvIMGoB6bvAX1OS0yY1i28RSYLvlM7sLUFeW2VgiKk+ORaHRRqAeW3p2hQ2umq8qlyy5dQKnHclToPOZeRrNdIHIZcIgLsLD8iEptD3NN5hL6DnEltJX2QbSH1RvjyRgt7ioSyuUE9gdWTi1k3AFuxCZnUh9c4zZjBJCT8G1mdXcbyc04GutoPm4TSoDt0N8o5/G7FSIvqY3JUf45u46oXIOQD9leyif75n2BTIt2PI/ug78ido8Jh1ZgYTzenYWz0nSZTe+9dJbMF+5gOhuG/mBPRX4ZArpIOfz"
-
-# dynamodb = boto3.resource('dynamodb',
-#                           aws_access_key_id=ACCESS_KEY,
-#                           aws_session_token=SESSION_TOKEN,
-#                           aws_secret_access_key=SECRET_KEY,
-#                           region_name='us-east-1')
-# table = dynamodb.Table('Tickets')
-
-# response = table.update_item(
-#         Key={'TicketId': 'idddddddd'},
-#         AttributeUpdates={'status': {
-#             'Value': 'finito',
-#             'Action': 'PUT'
-#         }},
-#         ReturnValues="UPDATED_NEW")
-
-
 def process_image(filename, speed, timestamp):
     #
     # Storing image to cloud bucket
@@ -121,10 +94,3 @@ def process_image(filename, speed, timestamp):
     print license_plate
     # initiate fine procedure
     process_fine(license_plate, timestamp, speed, BUCKET_URL + filename)
-
-
-# process_image()
-
-# process_image('lambo1.jpg', '60', '101010101')
-
-# send_email_for_fine('saumyaj@live.com', '10101', '75', '0.75')
