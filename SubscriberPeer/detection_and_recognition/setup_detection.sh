9fccd41583427f1c95cf1024ec608f0736c8cd12
Omkar <ovd208@nyu.edu>
2020-05-22 19:03:45 -0400

    refactoring

--- SubscriberPeer/detection_and_recognition/setup_detection.sh
+++
@@ -1,4 +0,0 @@
-conda env create -f environment.yml
-file_path='https://drive.google.com/uc?id=1X3cdEYdrzsOKkEOwfczYqYZ0rwxy9O_s'
-gdown $file_path
-unzip yolo-weights-for-licence-plate-detector.zip
